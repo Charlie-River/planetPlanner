85b718e8fe37ccb0e7c9b8c78e4e0823b76a18a5
Charlie-River <charlie.river19@gmail.com>
2024-02-26 14:50:36 +0000

    Updated query to fetch task information AND current folder information

--- fetch_tasks.php
+++ fetch_tasks.php
@@ -6,7 +6,8 @@ require_once("connectdb.php");
 if (isset($_POST['folderId'])) {
     $folderId = $_POST['folderId'];
 
-    $query = $db->prepare("SELECT * FROM tasks WHERE folder_id = ?");
+    $query = $db->prepare("SELECT * FROM tasks JOIN folders ON tasks.folder_id = folders.folder_id WHERE tasks.folder_id = ?");
+
     $query->execute([$folderId]);
     $tasks = $query->fetchAll(PDO::FETCH_ASSOC);
 
