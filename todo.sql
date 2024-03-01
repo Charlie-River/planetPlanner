a98957ad1d46667b78b5ebbaa70dfaf97f560086
Charlie :) <74555580+Charlie-River@users.noreply.github.com>
2024-03-01 22:18:39 +0000

    Update todo.sql
    Updated: adding a completed at column so that tasks will be deleted automatically after a specific period of time

--- todo.sql
+++ todo.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 28, 2023 at 02:09 PM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.2.0
+-- Generation Time: Mar 01, 2024 at 11:22 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -42,7 +42,11 @@ INSERT INTO `folders` (`folder_id`, `user_id`, `folderName`, `created_at`) VALUE
 (1, 4, 'new folder', '2023-12-18 18:37:20'),
 (2, 4, 'folder', '2023-12-18 18:38:37'),
 (3, 4, 'School Folder', '2023-12-18 18:46:35'),
-(4, 4, 'Final Folder', '2023-12-19 01:38:10');
+(4, 4, 'Final Folder', '2023-12-19 01:38:10'),
+(8, 5, 'my folder', '2024-02-24 12:58:57'),
+(17, 5, 'A NEW FOLDER', '2024-02-25 20:55:20'),
+(18, 6, 'Folder 1', '2024-02-29 20:08:42'),
+(19, 6, 'Folder 2', '2024-02-29 20:08:48');
 
 -- --------------------------------------------------------
 
@@ -57,17 +61,27 @@ CREATE TABLE `tasks` (
   `taskDescription` text DEFAULT NULL,
   `dueDate` date DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `completed` tinyint(1) NOT NULL DEFAULT 0
+  `completed` tinyint(1) NOT NULL DEFAULT 0,
+  `completed_at` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `tasks`
 --
 
-INSERT INTO `tasks` (`task_id`, `folder_id`, `taskName`, `taskDescription`, `dueDate`, `created_at`, `completed`) VALUES
-(1, 3, 'First Task', 'First Task Description', NULL, '2023-12-19 01:36:23', 0),
-(2, 3, 'Second Task', 'Second Task Desc', NULL, '2023-12-20 18:18:36', 0),
-(3, 1, 'new folder task 1', 'this should be in a different folder', NULL, '2023-12-28 10:03:29', 0);
+INSERT INTO `tasks` (`task_id`, `folder_id`, `taskName`, `taskDescription`, `dueDate`, `created_at`, `completed`, `completed_at`) VALUES
+(1, 3, 'First Task', 'First Task Description', NULL, '2023-12-19 01:36:23', 0, NULL),
+(2, 3, 'Second Task', 'Second Task Desc', NULL, '2023-12-20 18:18:36', 0, NULL),
+(3, 1, 'new folder task 1', 'this should be in a different folder', NULL, '2023-12-28 10:03:29', 0, NULL),
+(4, 8, 'Test Users First Task', 'Created using the localhost - local', NULL, '2024-02-25 19:24:55', 0, NULL),
+(5, NULL, 'second', 'form', NULL, '2024-02-25 20:11:16', 0, NULL),
+(6, NULL, 'task', 'form', NULL, '2024-02-25 20:16:27', 0, NULL),
+(7, NULL, 'furst', 'Created using form', NULL, '2024-02-25 20:20:20', 0, NULL),
+(9, 8, 'first task', 'Created using form', NULL, '2024-02-25 20:52:30', 0, NULL),
+(10, 8, 'IT WORKS?', 'dOES THIS ACTUALLY WORK', NULL, '2024-02-25 20:54:37', 0, NULL),
+(11, 8, 'TESTING TASK LIMIT', 'TOO MANY TASKS BAD??', NULL, '2024-02-25 20:55:02', 0, NULL),
+(12, 17, 'this should be different', 'new task', NULL, '2024-02-25 20:55:34', 0, NULL),
+(23, 18, 'Final test', 'hopefully', NULL, '2024-03-01 17:06:24', 1, '2024-03-01');
 
 -- --------------------------------------------------------
 
@@ -89,7 +103,9 @@ INSERT INTO `users` (`user_id`, `username`, `password`) VALUES
 (1, 'charlie', 'pass'),
 (2, 'c', '$2y$10$XlFKKsL/VTuch4yI9LPbeeRCU6TzYSFst.8qkqfa0Av.z331AHXHu'),
 (3, 'tester', '$2y$10$OsXgNdqhnyOAcWRcERuUPuo8/AsdR0WgEyBl06hIZKDnth02NpcHS'),
-(4, 'chloe', '$2y$10$HPuIL9FKQDWLaJFQI27rCuAcfLQ6kJR0plIC0l1YTQtIZuAAf0UI6');
+(4, 'chloe', '$2y$10$HPuIL9FKQDWLaJFQI27rCuAcfLQ6kJR0plIC0l1YTQtIZuAAf0UI6'),
+(5, 'testuser', '$2y$10$RkXk105pAvxlXzHgQK.46u6BXSMy2gQ5ECjubN8kozpk1SIFqIfH6'),
+(6, 'charlie-admin', '$2y$10$Zx8kvgxQbOOffJtygFi2VOkS3QXaJh7qc2LHeINCIIUz.J3xVihka');
 
 --
 -- Indexes for dumped tables
@@ -123,19 +139,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `folders`
 --
 ALTER TABLE `folders`
-  MODIFY `folder_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `folder_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `tasks`
 --
 ALTER TABLE `tasks`
-  MODIFY `task_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `task_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Constraints for dumped tables
